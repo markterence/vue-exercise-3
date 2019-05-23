876d338dd8f1fa367bc84eddf6a87cc5cd377577
Mark Terence <mtiglao.ccheetah@gmail.com>
2019-05-23 16:38:52 +0800

    moved resources

---
+++ src/components/Resources.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<h3>Resources</h3>
+    <ul class="list-group">
+      
+      <li>
+        <h5>Scoped Slots</h5>
+        <ul class="list-group">
+          <li><a href="https://adamwathan.me/the-trick-to-understanding-scoped-slots-in-vuejs/">The Trick to Understanding Scoped Slots in Vue.js</a></li>
+          <li><a href="https://vuejs.org/v2/guide/components-slots.html#Scoped-Slots">Scoped Slots</a></li> 
+        </ul>
+      </li>
+
+      <li>
+        <h5>Events</h5>
+        <ul class="list-group">
+          <li><a href="https://vuejs.org/v2/guide/components-custom-events.html">Custom Events</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <h5><span class="monospace">$refs</span></h5>
+        <ul class="list-group">
+          <li><a href="https://vuejs.org/v2/guide/components-edge-cases.html#Element-amp-Component-Access">Element &amp; Component Access</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    <div class="d-flex justify-content-center align-items-center mt-5">
+      <span class="mr-2"> Github Repo: </span>
+      <a target="_blank" href="https://github.com/markterence/vue-exercise-3" style="color: #e83e8c;">
+        <code>https://github.com/markterence/vue-exercise-3</code>
+      </a>
+    </div>
+	</div>
+</template>
